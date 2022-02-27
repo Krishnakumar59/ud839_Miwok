8da53b31cc9e00f554d92c1d9a489522347b877d
Krishna Kumar Mukhiya <Krishnatrappist@gmail.com>
2022-02-28 03:12:46 +0530

    Implemented ArrayList

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -3,6 +3,11 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+//        Adding number
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        LinearLayout rootLayout = (LinearLayout)findViewById(R.id.rootView);
+        TextView textView = new TextView(this);
+        for (int i = 0; i <words.size() ; i++) {
+            textView.setText(words.get(i));
+        }
+        rootLayout.addView(textView);
     }
 }
